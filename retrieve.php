df610c7472835d5ade67aba62861560e13156bf0
E-Andam <emmanuela.andam@ashesi.edu.gh>
2022-10-28 12:30:37 +0000

    add all

--- retrieve.php
+++ retrieve.php
@@ -10,31 +10,30 @@ $TankID = " ";
 $Reading = " ";
 
 if (isset($_GET['TankID'])) {
-	$TankID = $_GET['TankID'];
+    $TankID = $_GET['TankID'];
 }
 
 if (isset($_GET['Reading'])) {
-	$Reading = $_GET['Reading'];
+    $Reading = $_GET['Reading'];
 }
-    
-    if(isset($_GET['Owner'])){
-    $Owner=$_GET['Owner'];
-    }
-    
-    if(isset($_GET['Location'])){
-    $Location=$_GET['Location'];
-    }
-    
-    
-    $data=array();        
 
-	$sql_latest = mysqli_query($con,"SELECT TankInformation.TankID,TankSystem.Reading, TankInformation.Owner,TankInformation.Location FROM TankInformation,TankSystem WHERE TankInformation.TankID = TankSystem.TankID ORDER BY TankSystem.Time DESC LIMIT 1");
-    
-    $row=mysqli_fetch_object($sql_latest);
-    while ($row)
-    {         
-        
-        echo " 
+if (isset($_GET['Owner'])) {
+    $Owner = $_GET['Owner'];
+}
+
+if (isset($_GET['Location'])) {
+    $Location = $_GET['Location'];
+}
+
+
+$sql = "SELECT TankInformation.TankID,TankSystem.Reading, TankInformation.Owner,TankInformation.Location FROM TankInformation,TankSystem WHERE TankInformation.TankID = TankSystem.TankID ORDER BY TankSystem.Time DESC LIMIT 1";
+$result = mysqli_query($con, $sql);
+
+
+
+$row = mysqli_fetch_object($result);
+while ($row) {
+    echo " 
         <table style='width:100%'>
         <tr>
             <td> {$row->TankID}</td> 
@@ -43,7 +42,6 @@ if (isset($_GET['Reading'])) {
             <td> {$row->Location}</td> 
         </tr>
         ";
-        $row=mysqli_fetch_object($q);
-    }
-    echo " </table>";
-
+    $row = mysqli_fetch_object($q);
+}
+echo " </table>";
